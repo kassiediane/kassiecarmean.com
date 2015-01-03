59f6a48128209a4e678ba6d0584cf3076c10d1a7
kassiediane <kassie.carmean@gmail.com>
2015-01-03 10:16:06 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html> 
+  <head>
+    <meta charset="utf-8">
+    <title>Kassie Carmean | Front-end Web Developer</title> 
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:400,600,400italic,600italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Sacramento' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Just+Me+Again+Down+Here' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Dawning+of+a+New+Day' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Tienne:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Laila:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+  </head>
+  <body>
+    <header>
+      <a href="index.php" id="logo"> 
+      <h1>Kassie Carmean</h1>
+      <h2>Front-end Web Developer</h2>
+      </a>
+      <nav>
+        <ul>
+        <li><a href="about.php">About</a></li>
+        <li><a href="index.php" class="selected">Portfolio</a></li>
+        <li><a href="contact.php">Contact</a></li>
+       </ul>
+      </nav>
+      </header>
+    <div id="wrapper">
+        
+<!--
+      <section>
+          <ul id="gallery">
+          <li>
+            <a href="quote.jpg">
+                <div class="container">
+              <img src="https://img1.etsystatic.com/000/0/5812274/il_570xN.291314047.jpg" alt=""></div>
+              <p>Projects to be added soon!</p>
+            </a>
+           </li>
+            <li>
+            <a href="quote.jpg">
+                <div class="container">
+              <img src="http://jackmaloneyphotography.com/wp-content/gallery/print-images/mg_2959.jpg" alt="">
+                </div>
+              <p>Projects to be added soon!</p>
+            </a>
+           </li>
+          </ul>
+        </section>
+      
+--!>
+<!--WORK IN PROGRESS--!>
+<section id="Temporary">
+<center> Hi! Thanks so much for visiting my page. My portfolio is in progress, check back soon! In the meantime, feel free to check out the rest of my website!</center>
+</section>
+        <?php include('footer.php'); ?>
+      </div>
+  </body>  
+</html>
